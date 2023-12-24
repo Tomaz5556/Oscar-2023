cba2e44644666c8f3cb5d4facc3f6fcd15d5c48e
Tomaz5556 <tmb5@aluno.ifnmg.edu.br>
2023-12-24 18:35:10 -0300

    AsyncStorage 1

--- oscar-2023/src/AsyncStorageOperations.tsx
+++ oscar-2023/src/AsyncStorageOperations.tsx
@@ -1,34 +1,24 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const storeData = async (key: string, value: any) => {
-    try {
-      const existingData = await getData(key);
-      let newData = [];
-      if (existingData) {
-        newData = [...existingData, value];
-      } else {
-        newData = [value];
-      }
-      const jsonValue = JSON.stringify(newData);
-      await AsyncStorage.setItem(key, jsonValue);
-      console.log('Dados armazenados: ', newData);
-    } catch (e) {
-      // saving error
-      console.log(e);
-    }
+  try {
+    const jsonValue = await AsyncStorage.getItem('users');
+    let usersData = jsonValue != null ? JSON.parse(jsonValue) : [];
+    let userData: { [key: string]: any } = {};
+    userData[key] = value;
+    usersData.push(userData);
+    await AsyncStorage.setItem('users', JSON.stringify(usersData));
+    console.log('Dados armazenados: ', usersData);
+  } catch (e) {
+    console.log(e);
+  }
 }
-   
 
-export const getData = async (key: string) => {
-    try {
-      const jsonValue = await AsyncStorage.getItem(key)
-      if (jsonValue != null) {
-        console.log('Dados recuperados: ', JSON.parse(jsonValue));
-        return JSON.parse(jsonValue);
-      }
-      return null;
-    } catch(e) {
-      // error reading value
-      console.log(e);
-    }
-  }   
+export const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('users');
+    return jsonValue != null ? JSON.parse(jsonValue) : [];
+  } catch(e) {
+    console.log(e);
+  }
+}
