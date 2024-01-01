ee8f08006d1afcffaab21b8e46eb1388bd66c937
Tomaz5556 <tmb5@aluno.ifnmg.edu.br>
2024-01-01 18:43:09 -0300

    Correção Armazenamento dos Dados

--- oscar-2023/src/store/index.tsx
+++ oscar-2023/src/store/index.tsx
@@ -8,25 +8,20 @@ export const storeData = async (key: string, value: any) => {
     let userData: { [key: string]: any } = {};
     userData[key] = value;
 
-    if (usersData.length >= 2) {
-      let lastData = usersData[usersData.length - 1];
-      let penultimateData = usersData[usersData.length - 2];
-
-      let exists = Object.keys(lastData).some(k => k === key);
-      let penultimateExists = Object.keys(penultimateData).some(k => k === key);
-
-      if (exists) {
-        lastData[key] = value;
-      } else if (penultimateExists) {
-        penultimateData[key] = value;
-      } else {
-        usersData.push(userData);
-        console.log('Dados armazenados: ', usersData);
-      }
+    if (usersData.length === 0 || Object.keys(usersData[usersData.length - 1]).length === 4) {
+      let newUser: { [key: string]: any } = {};
+      newUser[key] = value;
+      usersData.push(newUser);
     } else {
-      usersData.push(userData);
+      let lastUser = usersData[usersData.length - 1];
+      if (!lastUser.hasOwnProperty(key)) {
+        lastUser[key] = value;
+      } else {
+        lastUser[key] = value;
+      }
     }
 
+    console.log('Dados armazenados: ', usersData);
     await AsyncStorage.setItem('users', JSON.stringify(usersData));
   } catch (e) {
     console.log(e);
